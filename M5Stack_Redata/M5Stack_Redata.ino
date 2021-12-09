543671c2fd204bbb322479eea51c194628fec40f
yagikun <yagikundayo@gmail.com>
2021-12-09 21:52:03 +0900

    現在記憶されているデータを返すようにした

--- M5Stack_Redata/M5Stack_Redata.ino
+++ M5Stack_Redata/M5Stack_Redata.ino
@@ -314,7 +314,6 @@ StampWord stampWord;
  */
 void setup() {
   M5.begin(); 
-  M5.Power.begin();
 
   Serial.begin(115200);         // Initialize serial communications with the PC
   while (!Serial);            // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
